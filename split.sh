8ca9f3a97d461645f7e59b46c0bc69f85f35c836
Lyx_xjtu <1115137437@qq.com>
2023-11-02 16:41:15 +0800

    fix bug

--- split.sh
+++ split.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-if [ ! $# == 2 ]; then
+if [ ! $# == 1 ]; then
 
-echo "Usage: $0 DataPath BatchNumber"
+echo "Usage: $0 DataPath"
 # /home/ms-admin/yixuan/data 1
 exit
 
@@ -11,9 +11,9 @@ fi
 
 filename="$1"
 
-batchnum="$2"
+rm -rf split
 
-python3 split.py "$filename" "$batchnum"
+python3 split_New.py "$filename"
 # python3 sample_multipro.py
 # python3 rf_multipro.py
 
